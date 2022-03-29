4a0f4fe0ada6e67d48856986aa5f21aa44f151f2
Daniel Lautzenheiser <lautzd@gmail.com>
2022-03-29 16:23:46 -0400

    Make decrypt runnable

--- imports/ui/Home.tsx
+++ imports/ui/Home.tsx
@@ -1,12 +1,12 @@
-import { Box } from "@mui/material";
-import React from "react";
-import { Containers } from "./containers/Containers";
-import { Header } from "./Header";
-import { Plants } from "./plants/Plants";
+import { Box } from '@mui/material';
+import React from 'react';
+import { Containers } from './containers/Containers';
+import { Header } from './Header';
+import { Plants } from './plants/Plants';
 
 export const Home = () => {
   return (
-    <Box sx={{ display: "flex", flexDirection: 'column', alignItems: "center", width: "100%" }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
       <Header />
       <Plants />
       <Containers />
