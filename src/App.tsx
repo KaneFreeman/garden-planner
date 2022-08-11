98a3429487092acad85fc823b81df0bf64d08fdf
Daniel Lautzenheiser <lautzd@gmail.com>
2022-08-11 11:32:16 -0400

    Fix environment variable names

--- src/App.tsx
+++ src/App.tsx
@@ -59,8 +59,8 @@ const App = () => {
 
   return (
     <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN ?? ''}
-      clientId={process.env.AUTH0_CLIENT_ID ?? ''}
+      domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ''}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ''}
       redirectUri={window.location.origin}
     >
       <ThemeProvider theme={theme}>
