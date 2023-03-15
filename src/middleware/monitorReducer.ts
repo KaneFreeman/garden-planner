37e1bba19e64b5f4dbd1b2c48d46664585c112eb
Daniel Lautzenheiser <lautzd@gmail.com>
2023-03-15 09:18:51 -0400

    Switch to yarn

--- src/middleware/monitorReducer.ts
+++ src/middleware/monitorReducer.ts
@@ -1,24 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Reducer } from 'react';
-import { PreloadedState, StoreEnhancer } from 'redux';
+import { StoreEnhancer } from 'redux';
 
 const round = (number: number) => Math.round(number * 100) / 100;
 
-const monitorReducerEnhancer: StoreEnhancer =
-  (createStore) => (reducer: Reducer<any, any>, preloadedState?: PreloadedState<any> | undefined) => {
-    const monitoredReducer: Reducer<any, any> = (state, action) => {
-      const start = performance.now();
-      const newState = reducer(state, action);
-      const end = performance.now();
-      const diff = round(end - start);
+const monitorReducerEnhancer: StoreEnhancer = (createStore) => (reducer: Reducer<any, any>, preloadedState?: any) => {
+  const monitoredReducer: Reducer<any, any> = (state, action) => {
+    const start = performance.now();
+    const newState = reducer(state, action);
+    const end = performance.now();
+    const diff = round(end - start);
 
-      // eslint-disable-next-line no-console
-      console.info('reducer process time:', diff);
+    // eslint-disable-next-line no-console
+    console.info('reducer process time:', diff);
 
-      return newState;
-    };
-
-    return createStore(monitoredReducer, preloadedState);
+    return newState;
   };
+
+  return createStore(monitoredReducer, preloadedState);
+};
 
 export default monitorReducerEnhancer;
